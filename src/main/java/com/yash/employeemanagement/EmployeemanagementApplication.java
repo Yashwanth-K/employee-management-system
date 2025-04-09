3cefd7904b57a6e54cfa2293a621cb29bb2a2e55
Yashwanth-k <yashk.edu@gmail.com>
2025-04-09 13:41:42 +0000

    Updated main class to enable cache

--- src/main/java/com/yash/employeemanagement/EmployeemanagementApplication.java
+++ src/main/java/com/yash/employeemanagement/EmployeemanagementApplication.java
@@ -2,8 +2,10 @@ package com.yash.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EmployeemanagementApplication {
 
 	public static void main(String[] args) {
